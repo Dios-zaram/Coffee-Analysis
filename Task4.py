2ca98f3f40be9ba9d447999d0cf10ca98dd7a531
Dios-zaram <141500635+Dios-zaram@users.noreply.github.com>
2023-08-18 10:37:24 -1100

    Update Task4.py

--- Task4.py
+++ Task4.py
@@ -3,15 +3,12 @@
 
 #Answer
 
-## The scatter plot will allow you to demonstrate the relationship between the type of store and the number of reviews, providing insights into how the number of reviews varies across different types of stores.
 
 import matplotlib.pyplot as plt
 import pandas as pd
 
 df = pd.read_csv("coffee_csv.csv")
 df=pd.DataFrame(df)
-# Assuming df is your DataFrame containing the data
-# If 'Reviews' is not already in numeric format, convert it to numeric
 
 df['Reviews'] = pd.to_numeric(df['Reviews'], errors='coerce')
 
@@ -33,10 +30,3 @@ plt.tight_layout()
 
 plt.show()
 
-# Interpreting the Visualization:
-# The scatter plot will display individual data points, where each point represents a place with its corresponding number of reviews.
-# The x-axis represents the different types of stores (place_type), and the y-axis represents the number of reviews for each place.
-
-# Based on the visualization:
-# You can observe the distribution of reviews for each type of store.
-# Look for patterns, trends, or any differences in the number of reviews among different types of stores.
