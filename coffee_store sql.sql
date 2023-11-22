235b06b6993d3336ae32b5b44d7327218daa3aad
Dios-zaram <141500635+Dios-zaram@users.noreply.github.com>
2023-11-22 10:47:09 -1100

    Update coffee_store sql.sql

--- coffee_store sql.sql
+++ coffee_store sql.sql
@@ -11,73 +11,167 @@ dine_in_option BOOL,
 takeout_option BOOL);
 
 --insert values
-COPY coffee_store FROM 'coffee_csv1.csv'
+COPY coffee_store FROM 'C:\Program Files\PostgreSQL\15\data\data_copy\coffee_csv1.csv'
 DELIMITER ',' CSV HEADER;
 
-SELECT * FROM coffee_store;
-
--- Check if values match the description
+-- The entire dataset
 SELECT 
-    CASE 
-        WHEN region IN ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J') THEN 'Match'
-        ELSE 'Does not match'
-    END AS region_description_match,
-    CASE 
-        WHEN place_name IS NOT NULL THEN 'Match'
-        ELSE 'Does not match'
-    END AS place_name_description_match,
-    CASE 
-        WHEN place_type IN ('Coffee shop', 'Cafe', 'Espresso bar', 'Others') THEN 'Match'
-        ELSE 'Does not match'
-    END AS place_type_description_match,
-    CASE 
-        WHEN rating BETWEEN 3 AND 5 THEN 'Match'
-        ELSE 'Does not match'
-    END AS rating_description_match,
-    CASE 
-        WHEN reviews IS NOT NULL THEN 'Match'
-        ELSE 'Does not match'
-    END AS review_description_match
+	*
 FROM coffee_store;
 
---Count the number of missing values
-SELECT 
-    COUNT(*) AS region_missing_count,
-    COUNT(*) AS place_name_missing_count,
-    COUNT(*) AS place_type_missing_count,
-    COUNT(*) AS rating_missing_count,
-    COUNT(*) AS review_missing_count
-FROM coffee_store
-WHERE region IS NULL OR place_name IS NULL OR place_type IS NULL OR rating IS NULL OR reviews IS NULL;
+-- Update dataset
+UPDATE
+	coffee_store SET 
+dine_in_option =
+	CASE WHEN dine_in_option IS null THEN false ELSE true END,
+takeout_option =
+	CASE WHEN takeout_option IS null THEN false ELSE true END;
 
---Insert missing values that is null
-INSERT INTO coffee_store (rating, reviews, dine_in_option, takeout_option)
+-- Delete null from rating column
+DELETE 
+	FROM coffee_store 
+WHERE rating IS null;
+
+/*Rating analysis*/
+-- Find cummulative frequency of rating
 SELECT 
-    COALESCE(rating, 0),
-    COALESCE(reviews, 271.5),
-    COALESCE(dine_in_option, 'false'),
-    COALESCE(takeout_option, 'false')
+    rating,
+    SUM(CASE WHEN place_type = 'Cafe' AND rating BETWEEN 3.9 AND 5.0 THEN 1 ELSE 0 END) AS cafe_cumulative_frequency,
+    SUM(CASE WHEN place_type = 'Coffee shop' AND rating BETWEEN 3.9 AND 5.0 THEN 1 ELSE 0 END) AS coffee_shop_cumulative_frequency,
+    SUM(CASE WHEN place_type = 'Espresso bar' AND rating BETWEEN 3.9 AND 5.0 THEN 1 ELSE 0 END) AS espresso_bar_cumulative_frequency,
+    SUM(CASE WHEN place_type = 'Others' AND rating BETWEEN 3.9 AND 5.0 THEN 1 ELSE 0 END) AS others_cumulative_frequency
 FROM 
     coffee_store
-WHERE 
-    rating IS NULL OR reviews IS NULL OR dine_in_option IS NULL OR takeout_option IS NULL;
+GROUP BY 
+    rating;
 
---Which category of the variable place type have the most observations
-SELECT place_type, COUNT(*) AS store_count
-FROM coffee_store
-GROUP BY place_type
-ORDER BY store_count DESC;
-
---The distribution of all of the number of reviews.
+/*Central tendency of rating*/
+-- Find the Average
 SELECT 
-    MIN(reviews) AS min_reviews,
-    MAX(reviews) AS max_reviews,
-    AVG(reviews) AS avg_reviews,
-    MEDIAN(reviews) AS median_reviews,
-    STDDEV(reviews) AS stddev_reviews
+	AVG(rating)
 FROM coffee_store;
 
---The relationship between type of store and number of reviews
-SELECT place_type, AVG(review) AS avg_reviews
-FROM coffee_store
-GROUP BY place_type;
+-- Find Median
+SELECT 
+	PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY rating) median
+FROM coffee_store;
+
+-- Find Mode
+SELECT
+  MODE() WITHIN GROUP (ORDER BY rating)
+FROM
+  coffee_store;
+
+-- Find Variance
+SELECT 
+	VAR_SAMP(rating)
+FROM coffee_store;
+
+-- Find Standard Deviation
+SELECT 
+	STDDEV_SAMP(rating)
+FROM coffee_store;
+
+-- Number of rating
+SELECT 
+	COUNT(rating)
+FROM coffee_store;
+
+-- Find Standard error
+SELECT
+  ROUND((STDDEV_SAMP(rating) / SQRT(COUNT(rating)))::numeric,2)
+FROM coffee_store;
+
+-- Find the upper and lower confidence interval
+SELECT
+  AVG(rating) - 1.96 * (STDDEV_SAMP(rating) / SQRT(COUNT(rating))) AS lower_limit,
+  AVG(rating) + 1.96 * (STDDEV_SAMP(rating) / SQRT(COUNT(rating))) AS upper_limit
+FROM
+  coffee_store;
+
+/*      OBSERVATION
+The mean rating for the dataset is 4.66, and the calculated 95% confidence interval
+for the mean is approximately [4.6327,4.6945]. This interval suggests that, with 
+95% confidence, the true population mean rating is likely to fall within this 
+range.*/
+
+
+/*Frequency*/
+-- Frequency for delivery_option
+SELECT 
+    delivery_option,
+    SUM(CASE WHEN place_type = 'Cafe' AND delivery_option= TRUE THEN 1 ELSE 0 END) AS cafe_cumulative_frequency,
+    SUM(CASE WHEN place_type = 'Coffee shop' AND delivery_option =TRUE THEN 1 ELSE 0 END) AS coffee_shop_cumulative_frequency,
+    SUM(CASE WHEN place_type = 'Espresso bar' AND delivery_option =TRUE THEN 1 ELSE 0 END) AS espresso_bar_cumulative_frequency,
+    SUM(CASE WHEN place_type = 'Others' AND delivery_option =TRUE THEN 1 ELSE 0 END) AS others_cumulative_frequency
+FROM 
+    coffee_store
+GROUP BY 
+	delivery_option
+ORDER BY
+	delivery_option desc;
+
+
+SELECT 
+    dine_in_option,
+    SUM(CASE WHEN place_type = 'Cafe' AND dine_in_option= TRUE THEN 1 ELSE 0 END) AS cafe_cumulative_frequency,
+    SUM(CASE WHEN place_type = 'Coffee shop' AND dine_in_option =TRUE THEN 1 ELSE 0 END) AS coffee_shop_cumulative_frequency,
+    SUM(CASE WHEN place_type = 'Espresso bar' AND dine_in_option =TRUE THEN 1 ELSE 0 END) AS espresso_bar_cumulative_frequency,
+    SUM(CASE WHEN place_type = 'Others' AND dine_in_option =TRUE THEN 1 ELSE 0 END) AS others_cumulative_frequency
+FROM 
+    coffee_store
+GROUP BY 
+	dine_in_option;
+	
+SELECT 
+    takeout_option,
+    SUM(CASE WHEN place_type = 'Cafe' AND takeout_option= TRUE THEN 1 ELSE 0 END) AS cafe_cumulative_frequency,
+    SUM(CASE WHEN place_type = 'Coffee shop' AND takeout_option =TRUE THEN 1 ELSE 0 END) AS coffee_shop_cumulative_frequency,
+    SUM(CASE WHEN place_type = 'Espresso bar' AND takeout_option =TRUE THEN 1 ELSE 0 END) AS espresso_bar_cumulative_frequency,
+    SUM(CASE WHEN place_type = 'Others' AND takeout_option =TRUE THEN 1 ELSE 0 END) AS others_cumulative_frequency
+FROM 
+    coffee_store
+GROUP BY 
+	takeout_option
+ORDER BY
+	takeout_option desc;
+	
+/*		OBSERVATION
+1. The data suggests that the "Coffee shop" category tends to have a high 
+frequency of  establishments offering both dine-in and take-out options.
+2. "Cafe" shows a higher frequency in offering delivery.
+3. The "Others" category seems to have varied offerings in terms of delivery, 
+dine-in, and take-out.
+*/
+
+/*Review Central Tendency*/
+  
+SELECT 
+    region,
+    MAX(reviews) AS max_reviews,
+    MIN(reviews) AS min_reviews,
+	MAX(rating) AS max_rating,
+    MIN(rating) AS min_rating
+FROM
+    coffee_store
+GROUP BY
+    region;
+
+
+/* OBSERVATIONS 
+
+1. The region with the highest maximum reviews is E, achieving a perfect rating
+of 5. In contrast, region C has the lowest maximum reviews (806), but it also 
+maintains a perfect rating of 5. Notably, region I is the only one with a maximum
+rating lower than 5.
+
+2. Recognizing the challenge of distinguishing between good and bad reviews, 
+less emphasis is placed on reviews in the analysis.
+
+3. Across regions, there is a consistent trend of a maximum rating of 5. To 
+capture subtle variations, the minimum rating for each region is considered, as 
+it offers greater accuracy.
+
+4. Region G stands out with the highest minimum rating of 4.6, while both 
+regions I and H share the lowest minimum rating.*/
+
